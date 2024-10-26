f7ef4dd02f9513f3229c9de301e3e80599ffc6db
hamdymohamedak <mohameddhamdy407@gmail.com>
2024-10-26 13:46:01 +0300

    egypt-day-Tours-sharm-elShekh page

--- Tours.js
+++ Tours.js
@@ -3776,83 +3776,257 @@ Breakfast, Lunch.
     ],
   },
 
-  // 00: {
-  //     title:
-  //       "",
-  //     maxGuests: "Unlimited",
-  //     location: "Egypt",
-  //     left: "1 days",
-  //     des: "",
-  //     reviews: "000 Reviews",
-  //     priceSale: "$000",
-  //     priceOriginal: "$000",
-  //     rating: 5,
-  //     tourPlans: [
-  //       {
-  //         title: "",
-  //         des: "",
-  //       },
+  53: {
+    title: "Cairo Tour from Sharm El Sheikh by Plane",
+    maxGuests: "Unlimited",
+    location: "Egypt",
+    left: "1 days",
+    des: `Embark on a thrilling journey from Sharm El Sheikh to Cairo by plane and explore the historical and cultural milestones of the greatest city on earth. Marvelous Egypt Travel provides an enriching experience with a professional Egyptologist tour guide and a private A/C vehicle during your tour. This one-day excursion offers a chance to discover the ancient Egyptian civilization at the Giza Pyramids, explore the Egyptian Museum’s treasures, and experience the vibrant Khan El Khalili Bazaar. Book this unforgettable day trip to Cairo and immerse yourself in its magical allure.`,
+    reviews: "464 Reviews",
+    priceSale: "$420",
+    priceOriginal: "$420",
+    rating: 5,
+    tourPlans: [
+      {
+        title: "Cairo Tour from Sharm El Sheikh by Plane",
+        des: `
+Morning:
+Pick-Up and Transfer:
 
-  //     ],
-  //     included: [
-  //       ""
-  //     ],
-  //     excluded: [
-  //       ""
-  //     ],
-  //   },
+Start your journey with a transfer from your hotel in Sharm El Sheikh to the airport for your flight to Cairo. Upon arrival, meet your private Egyptologist guide and begin your Cairo tour.
+Arrival in Cairo:
 
-  // 00: {
-  //     title:
-  //       "",
-  //     maxGuests: "Unlimited",
-  //     location: "Egypt",
-  //     left: "1 days",
-  //     des: "",
-  //     reviews: "000 Reviews",
-  //     priceSale: "$000",
-  //     priceOriginal: "$000",
-  //     rating: 5,
-  //     tourPlans: [
-  //       {
-  //         title: "",
-  //         des: "",
-  //       },
+Upon arrival at Cairo airport, meet your private tour guide who will accompany you throughout the day.
+Explore the Giza Pyramids Complex:
 
-  //     ],
-  //     included: [
-  //       ""
-  //     ],
-  //     excluded: [
-  //       ""
-  //     ],
-  //   },
+Discover the colossal art of the ancient Egyptians, including the three main pyramids, six little pyramids, the Great Sphinx, and the Valley Temple.
+The Great Pyramid:
 
-  // 00: {
-  //     title:
-  //       "",
-  //     maxGuests: "Unlimited",
-  //     location: "Egypt",
-  //     left: "1 days",
-  //     des: "",
-  //     reviews: "000 Reviews",
-  //     priceSale: "$000",
-  //     priceOriginal: "$000",
-  //     rating: 5,
-  //     tourPlans: [
-  //       {
-  //         title: "",
-  //         des: "",
-  //       },
+Marvel at the Great Pyramid of Pharaoh Khufu, the last surviving wonder of the ancient world, standing tall at 147 meters with its intricate chambers.
+The Great Sphinx:
 
-  //     ],
-  //     included: [
-  //       ""
-  //     ],
-  //     excluded: [
-  //       ""
-  //     ],
-  //   },
+Gaze upon the Great Sphinx, a magnificent limestone statue with the body of a lion and the head of Pharaoh Khafre, symbolizing wisdom and power.
+The Valley Temple:
+
+Visit the Valley Temple, a mortuary temple used for the mummification process of 4th dynasty pharaohs before their burial in the pyramids.
+Discover the Grand Egyptian Museum:
+
+Explore the latest addition to Egypt's rich cultural heritage, home to a vast collection of artifacts, including the Hanging Obelisk, the King Ramses II Statue, the 10 Statues of King Senusert, and more. (Note: A VIP ticket is available for additional access to exclusive artifacts.)
+Lunch at a Pyramid/Nile View Restaurant:
+
+Enjoy a delightful lunch with stunning views of the Pyramids or the Nile.
+Afternoon:
+Visit the Egyptian Museum:
+
+Tour the historic Egyptian Museum, showcasing an extensive collection of ancient Egyptian artifacts, located in the heart of Cairo since 1901 AD.
+Stroll Al Muizz Street:
+
+Wander along Al-Muizz Street, established in the 10th century AD, reflecting the golden era of Islamic Egypt and leading to the vibrant Khan El Khalili Bazaar.
+Explore Khan El Khalili Bazaar:
+
+Experience the bustling atmosphere of Khan El Khalili Bazaar, established between 1382 and 1389 AD, offering a plethora of unique artifacts and exquisite Islamic architecture.
+Evening:
+Transfer to Cairo Airport:
+
+Transfer to Cairo airport for your flight back to Sharm El Sheikh.
+Return to Your Hotel:
+
+Upon arrival in Sharm El Sheikh, you will be transferred back to your hotel in Sharm El Sheikh, ending your Cairo day trip.
+Meals:
+
+Lunch.
+Flight:
+
+From Sharm to Cairo and Return.
+`,
+      },
+    ],
+    included: [
+      "Pick-up service from your hotel in Sharm and return",
+      "Flight tickets from Sharm to Cairo and return.",
+      "All transfers by a private modern air-conditioned vehicle. ",
+      "Private Egyptologist guide during your tour. ",
+      "Entrance fees to all the mentioned sights.",
+      "Lunch at a Pyramid/Nile view restaurant. ",
+      "Mineral water onboard the vehicle. ",
+      "All service charges and taxes. ",
+    ],
+    excluded: [
+      "Any extras not mentioned in the program. ",
+      "Tipping",
+      "Optional Experiences available at an additional cost. ",
+    ],
+  },
+
+  54: {
+    title: "Luxor Day Trip from Sharm El Sheikh by Plane",
+    maxGuests: "Unlimited",
+    location: "Egypt",
+    left: "1 days",
+    des: `Experience the majestic beauty of Luxor with a one-day trip from Sharm El Sheikh by plane. Marvelous Egypt Travel, renowned as the best travel agency in Egypt, will provide an unforgettable journey with a professional Egyptologist tour guide and a private air-conditioned vehicle. This tour offers a unique opportunity to explore the ancient Egyptian civilization by visiting the Valley of the Kings, Karnak Temple, Queen Hatshepsut Temple, and the Colossi of Memnon. Book this fantastic day trip and create timeless memories in the legendary city of Luxor.`,
+    reviews: "322 Reviews",
+    priceSale: "$695",
+    priceOriginal: "$000",
+    rating: 5,
+    tourPlans: [
+      {
+        title: "Luxor Day Trip from Sharm El Sheikh by Plane",
+        des: `
+Pick-Up and Transfer:
+
+Your representative will pick you up from your hotel in Sharm El Sheikh to catch your flight to Cairo, followed by a direct flight to Luxor. Upon arrival, meet your Egyptologist guide and start your private tour of Luxor's attractions.
+Explore the Valley of the Kings:
+
+This sacred site on the west bank of the Nile opposite Luxor is a golden history book buried deep within a sacred hill. Home to 63 royal tombs of the greatest Pharaohs of the 18th, 19th, and 20th dynasties of the New Kingdom (1570–1050 BC), including Ramses II, Hatshepsut, Seti I, and Tutankhamen. The valley gained global fame with the discovery of Tutankhamen's tomb, making it one of the most famous archaeological sites on earth. It was declared a World Heritage Site in 1979, along with the entire Theban Necropolis.
+Visit Hatshepsut Temple:
+
+This beautiful temple, known as the "Holy of Holies," was constructed over 15 years on the golden rock hills of Deir El Bahari in 1479 BC. Built to honor the sun god Amun and immortalize the legacy of Queen Hatshepsut, one of the most successful pharaohs in ancient Egyptian history, it is the most preserved example of classical ancient Egyptian architecture.
+Admire the Colossi of Memnon:
+
+These two grand statues of Pharaoh Amenhotep III (1386 - 1349 BC) were built in 1350 BC to guard the entrance of his long-faded temple. The statues underwent numerous renovations and became known for their Vocal Phenomenon at dawn during the Greco-Roman period. The Colossi are adorned with mystical decorations depicting Amenhotep III's history, family, and the Nile god Hapi.
+Lunch Time:
+
+Enjoy a delicious lunch on an island, accompanied by a scenic boat trip on the Nile River. After lunch, you will continue your guided tour to visit:
+Discover Karnak Temple:
+
+This colossal temple complex, covering 247 acres, was built between 2000 BC and 30 BC during the Middle Kingdom to the Greco-Roman period. It served as the main religious center for worshipping the Theban triad of Amun, Mut, and Khonsu for over 1500 years. The complex features the Avenue of Sphinxes, the grand Hypostyle Hall, and numerous carvings, paintings, and shrines dedicated to various gods.
+Explore Luxor Temple:
+
+The Luxor Temple, the southern Sanctuary, was created to be a center of celebration and admiration. Built in 1400 BC, it is dedicated to the Theban triad of Amun, Mut, and Khonsu and to host the Opet festival for the rejuvenation of kingship. It became part of the UNESCO World Heritage site in 1979.
+Sharm El Sheikh
+
+Transfer to Luxor Airport for your flight to Cairo, then catch your direct flight back to Sharm El Sheikh. Upon arrival, you will be transferred to your hotel, concluding your Luxor day trip.
+Meals:
+
+Lunch.
+`,
+      },
+    ],
+    included: [
+      "Pick-up service from your hotel in Sharm El Sheikh and return.",
+      "Domestic flight tickets from Sharm to Cairo/ from Cairo to Luxor/ from Luxor to Cairo/ from Cairo to Sharm. ",
+      "All transfers by a private modern air-conditioned vehicle.",
+      "Private Egyptologist guide during your tour. ",
+      "Entrance fees to all the mentioned sights. ",
+      "Lunch meal during the tour. ",
+      "Water and soft drinks onboard the vehicle. ",
+      "All service charges and taxes. ",
+    ],
+    excluded: [
+      "Any extras not mentioned in the program. ",
+      "Tipping",
+      "Optional Experiences available at an additional cost. ",
+    ],
+  },
+
+  55: {
+    title: "Overnight Trip to Cairo and Luxor from Sharm by Flight",
+    maxGuests: "Unlimited",
+    location: "Egypt",
+    left: "2 days",
+    des: `Embark on a majestic two-day tour from Sharm El Sheikh to Cairo and Luxor by flight. Witness the timeless wonders of ancient Egypt, exploring Cairo’s Giza Pyramids and Egyptian Museum, followed by Luxor’s Karnak Temple, Valley of the Kings, and Hatshepsut Temple. Marvelous Egypt Travel, the best travel agency in Egypt, ensures top-notch service with experienced Egyptologist guides and private air-conditioned vehicles. Book this incredible excursion to live the adventure of your wildest dreams.`,
+    reviews: "360 Reviews",
+    priceSale: "$840",
+    priceOriginal: "$840",
+    rating: 5,
+    tourPlans: [
+      {
+        title: "Day 1 Cairo",
+        des: `
+Pick-Up and Transfer:
+
+Your representative will pick you up from your hotel in Sharm El Sheikh in a private air-conditioned vehicle for your flight to Cairo. Upon arrival, meet your Egyptologist guide to explore Cairo's attractions.
+Arrival in Cairo:
+
+Upon arrival at Cairo airport, meet your private tour guide who will accompany you throughout the day.
+Explore the Giza Pyramids Complex:
+
+Discover the colossal art of the ancient Egyptians, including the three main pyramids, six little pyramids, the Great Sphinx, and the Valley Temple.
+The Great Pyramid:
+
+Marvel at the Great Pyramid of Pharaoh Khufu, the last surviving wonder of the ancient world, standing tall at 147 meters with its intricate chambers.
+The Great Sphinx:
+
+Gaze upon the Great Sphinx, a magnificent limestone statue with the body of a lion and the head of Pharaoh Khafre, symbolizing wisdom and power.
+The Valley Temple:
+
+Visit the Valley Temple, a mortuary temple used for the mummification process of 4th dynasty pharaohs before their burial in the pyramids.
+Discover the Grand Egyptian Museum:
+
+Explore the latest addition to Egypt's rich cultural heritage, home to a vast collection of artifacts, including the Hanging Obelisk, the King Ramses II Statue, the 10 Statues of King Senusert, and more. (Note: A VIP ticket is available for additional access to exclusive artifacts.)
+Lunch at a Pyramid/Nile View Restaurant:
+
+Enjoy a delightful lunch with stunning views of the Pyramids or the Nile.
+Visit the Egyptian Museum:
+
+Tour the historic Egyptian Museum, showcasing an extensive collection of ancient Egyptian artifacts, located in the heart of Cairo since 1901 AD.
+Transfer to Cairo Airport:
+
+You will be driven to Cairo Airport for your flight to Luxor. Upon arrival, transfer to a 5-star hotel for an overnight stay.
+Meals:
+
+Lunch.
+Overnight:
+
+Luxor Hotel.
+Flight:
+
+From Sharm to Cairo and From Cairo to Luxor.
+`,
+      },
+      {
+        title: "Day 2 Luxor",
+        des: `
+Morning:
+
+Enjoy breakfast at your hotel, then your representative will pick you up from your hotel in a private air-conditioned vehicle to explore Luxor's attractions.
+Explore the Valley of the Kings:
+
+This sacred site on the west bank of the Nile opposite Luxor is a golden history book buried deep within a sacred hill. Home to 63 royal tombs of the greatest Pharaohs of the 18th, 19th, and 20th dynasties of the New Kingdom (1570–1050 BC), including Ramses II, Hatshepsut, Seti I, and Tutankhamen. The valley gained global fame with the discovery of Tutankhamen's tomb, making it one of the most famous archaeological sites on earth. It was declared a World Heritage Site in 1979, along with the entire Theban Necropolis.
+Visit Hatshepsut Temple:
+
+This beautiful temple, known as the "Holy of Holies," was constructed over 15 years on the golden rock hills of Deir El Bahari in 1479 BC. Built to honor the sun god Amun and immortalize the legacy of Queen Hatshepsut, one of the most successful pharaohs in ancient Egyptian history, it is the most preserved example of classical ancient Egyptian architecture.
+Admire the Colossi of Memnon:
+
+These two grand statues of Pharaoh Amenhotep III (1386 - 1349 BC) were built in 1350 BC to guard the entrance of his long-faded temple. The statues underwent numerous renovations and became known for their Vocal Phenomenon at dawn during the Greco-Roman period. The Colossi are adorned with mystical decorations depicting Amenhotep III's history, family, and the Nile god Hapi.
+Lunch Time:
+
+Enjoy a delicious lunch on an island, accompanied by a scenic boat trip on the Nile River. After lunch, you will continue your guided tour to visit:
+Discover Karnak Temple:
+
+This colossal temple complex, covering 247 acres, was built between 2000 BC and 30 BC during the Middle Kingdom to the Greco-Roman period. It served as the main religious center for worshipping the Theban triad of Amun, Mut, and Khonsu for over 1500 years. The complex features the Avenue of Sphinxes, the grand Hypostyle Hall, and numerous carvings, paintings, and shrines dedicated to various gods.
+Explore Luxor Temple:
+
+The Luxor Temple, the southern Sanctuary, was created to be a center of celebration and admiration. Built in 1400 BC, it is dedicated to the Theban triad of Amun, Mut, and Khonsu and to host the Opet festival for the rejuvenation of kingship. It became part of the UNESCO World Heritage site in 1979.
+Transfer:
+
+Transfer to Luxor airport for your flight to Cairo, then catch your direct flight back to Sharm El Sheikh. Upon arrival, your representative will transfer you to your hotel in Sharm.
+Meals:
+
+Breakfast, Lunch.
+Flight:
+
+From Luxor to Cairo and From Cairo to Sharm.
+`,
+      },
+    ],
+    included: [
+      "Pick-up service from your hotel in Sharm El Sheikh and return. ",
+      "Domestic flight tickets from Sharm to Cairo/ Cairo to Luxor/ Luxor to Cairo/ Cairo to Sharm. ",
+      "All transfers by a private modern air-conditioned vehicle. ",
+      "Private Egyptologist guide during your tour. ",
+      "Entrance fees to all the mentioned sights. ",
+      "One night accommodation at a 5-star hotel in Luxor. ",
+      "2 Lunch meals during tours in Cairo and Luxor. ",
+      "Mineral water and soft drinks onboard the vehicle. ",
+      "All service charges and taxes. ",
+    ],
+    excluded: [
+      "Any extras not mentioned in the program. ",
+      "Tipping",
+      "Optional Experiences available at an additional cost.",
+    ],
+  },
 
   // 00: {
   //     title:
