e884a06f437f00bdc96298592c6ce45f17e31ef8
hamdymohamedak <mohameddhamdy407@gmail.com>
2024-11-05 16:34:48 +0200

    add Elgouna Excursions

--- creator.js
+++ creator.js
@@ -2,45 +2,76 @@ const fs = require("fs");
 const path = "./Egypt-Day-Tours/tour-single.html";
 
 let Data = {
-  title: "Day Trip to Abu Simbel from Aswan",
+  title: "2 Days Aswan and Abu Simbel Tour from El Gouna",
   maxGuests: "Unlimited",
   location: "Egypt",
   left: "1 days",
-  des: `Embark on a fascinating day trip from Aswan to Abu Simbel, where the monumental grandeur of ancient Egypt awaits you. This tour, provided by Marvelous Egypt Travel -the premier travel agency in Egypt- includes the expertise of a private Egyptologist guide and a comfortable, air-conditioned vehicle. Experience the breathtaking Temples of Abu Simbel, marvel at their grandeur, and delve into the rich history of ancient Egyptian civilization. Conclude your adventure with a return to your hotel or Nile river cruise, enriched by the majesty of Egypt’s past.`,
-  reviews: "165 Reviews",
-  priceSale: "$50",
-  priceOriginal: "$50",
+  des: `Embark on a splendid two-day tour from El Gouna to the majestic attractions of Aswan and Abu Simbel, showcasing the timeless wonders of southern Egypt. Marvelous Egypt Travel, the best travel agency in Egypt, ensures a comfortable and enriching experience with private A/C vehicles and professional tour guides. This tour will take you to explore Aswan’s top tourist attractions, including the High Dam, the Unfinished Obelisk, and Philae Temple, with an overnight stay at a 5-star hotel in Aswan. The next day, visit the awe-inspiring temples of Abu Simbel. Book this tour to witness the grandeur of ancient Egypt.`,
+  reviews: "360 Reviews",
+  priceSale: "$215",
+  priceOriginal: "$215",
   rating: 5,
   tourPlans: [
     {
-      title: "Day Trip to Abu Simbel from Aswan",
+      title: "Day 1 Aswan",
       des: `
 Pick-Up and Transfer:
 
-Begin your journey with a pickup from your hotel or Nile cruise in Aswan. Travel by private vehicle to the awe-inspiring Temples of Abu Simbel.
-Explore the Temples of Abu Simbel:
+You will be picked up from your hotel in El Gouna by a private A/C vehicle and transferred to Luxor to catch the first-class train to Aswan. Upon arrival, you will start your Aswan tour with your private tour guide.
+Visit the Aswan High Dam:
 
-The two rock-cut temples at Abu Simbel, masterpieces of ancient Egyptian architecture, were constructed during the reign of King Ramses II around 1200 B.C. The first temple is dedicated to Ramses II himself, showcasing colossal statues and intricate carvings. The second temple honors Queen Nefertari, adorned with exquisite artwork and inscriptions. Discover the grandeur of these monuments and their historical significance.
-Lunch:
+Built in the 1960s and 1970s, the Aswan High Dam is one of the largest embankment dams in the world. It plays a crucial role in the economy and culture of Egypt.
+Explore the Unfinished Obelisk:
 
-After visiting Abu Simbel, return to Aswan for a delightful lunch at a local restaurant.
-Transfer Back:
+This is the largest known obelisk from ancient Egypt, commissioned by Queen Hatshepsut (1508-1458 BC). The obelisk showcases the carving techniques of the ancient Egyptians and is now an open-air museum.
+Lunch Time:
 
-Conclude your day with a return transfer to your hotel or Nile cruise in Aswan, ending your memorable trip to Abu Simbel.
+Enjoy lunch at a local restaurant in Aswan.
+Discover Philae Temple:
+
+Dedicated to the goddess Isis, Philae Temple was built in 280 BC during the reign of Ptolemy II. It was relocated to Agilkia Island by UNESCO to save it from the rising waters of the Nile.
+Check-in:
+
+Transfer to a 5-star hotel in Aswan for an overnight stay.
 Meals:
 
 Lunch.
+Overnight:
+
+Aswan Hotel.
+`,
+    },
+    {
+      title: "Day 2 Abu Simbel",
+      des: `
+Early Morning:
+
+Enjoy breakfast as you prepare for an early start.
+Visit the Abu Simbel Temples:
+
+These temples are part of the UNESCO World Heritage Site known as the "Nubian Monuments." The temples, built by Pharaoh Ramses the Great in 1200 BC, honor Ptah, Amun, Ra, and commemorate Ramses' victory at the Battle of Kadesh and his devotion to Queen Nefertari.
+Lunch Time:
+
+Return to Aswan for lunch at a local restaurant.
+Return to El Gouna:
+
+Catch the train to Luxor and then transfer back to your hotel in El Gouna, concluding your two-day tour.
+Meals:
+
+Breakfast, Lunch.
 `,
     },
   ],
   included: [
-    "Pick up service from your hotel in Aswan/ Nile Cruise and return. ",
-    "All transfers by a private modern air-conditioned vehicle. ",
-    "Private Egyptologist guides during your tours. ",
+    "Pick-up service from your hotel in El Gouna and return. ",
+    "2 lunch meals in Aswan. ",
+    "All transfers by a private modern air-conditioned vehicle.",
+    "Private Egyptologist guide during your tour.",
     "Entrance fees to all the mentioned sights. ",
-    "Lunch at a local restaurant. ",
-    "Mineral water on board the vehicle during the tour. ",
-    "All service charges and taxes.",
+    "One-night accommodation in Aswan at a 5-star Hotel. ",
+    "	Train tickets from Luxor to Aswan and return.",
+    "Mineral water and soft drinks onboard the vehicle. ",
+    "All service charges and taxes. ",
   ],
   excluded: [
     "Any extras not mentioned in the program. ",
@@ -144,7 +175,7 @@ fs.readFile(path, "utf8", (err, data) => {
   // Define a new filename
   let newFileName = Data.title.replace(/ /g, "-"); // Replace all spaces with hyphens
 
-  const newPath = `./Egypt-Day-Tours-Aswan-Excursions/${newFileName}.html`;
+  const newPath = `./Egypt-Day-Tours-ElGouna-Excursions/${newFileName}.html`;
   // Write the updated content to a new HTML file
   fs.writeFile(newPath, updatedData, "utf8", (err) => {
     if (err) {
