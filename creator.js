36faa55d709eeff6998492e65e0f599e3e7d3175
hamdymohamedak <mohameddhamdy407@gmail.com>
2024-11-05 16:59:00 +0200

    add paths at sharm elshekh

--- creator.js
+++ creator.js
@@ -2,82 +2,112 @@ const fs = require("fs");
 const path = "./Egypt-Day-Tours/tour-single.html";
 
 let Data = {
-  title: "Makadi Bay Excursion to Aswan and Abu Simbel in Two Days Tour",
-    maxGuests: "Unlimited",
-    location: "Egypt",
-    left: "2 days",
-    des: `The Makadi Bay excursion to Aswan & Abu Simbel over two days showcases the extraordinary legacy of the ancient Egyptian Civilization, brimming with glory and magnificence. Marvelous Egypt Travel, the top travel agency in Egypt, ensures guests receive excellent service, including a private A/C vehicle and a professional tour guide during their holiday tour in the legendary lands of Southern Egypt. Witness ancient visions of class and glory in Upper Egypt, including the Unfinished Obelisk and the majestic Philae Temple in Aswan, and the grand Abu Simbel temples at the southern border of Egypt. This tour offers a journey into the true meaning of ageless allure, allowing you to explore the pride of southern Egypt in full comfort and relaxation. Book this enchanting tour and dive into the beauty and history of Aswan and Abu Simbel.`,
-    reviews: "315 Reviews",
-    priceSale: "$185",
-    priceOriginal: "$185",
-    rating: 5,
-    tourPlans: [
-      {
-        title: "Day 1 Aswan",
-        des: `
+  title: "Overnight Trip to Cairo and Luxor from Sharm by Flight",
+  maxGuests: "Unlimited",
+  location: "Egypt",
+  left: "2 days",
+  des: `Embark on a majestic two-day tour from Sharm El Sheikh to Cairo and Luxor by flight. Witness the timeless wonders of ancient Egypt, exploring Cairo’s Giza Pyramids and Egyptian Museum, followed by Luxor’s Karnak Temple, Valley of the Kings, and Hatshepsut Temple. Marvelous Egypt Travel, the best travel agency in Egypt, ensures top-notch service with experienced Egyptologist guides and private air-conditioned vehicles. Book this incredible excursion to live the adventure of your wildest dreams.`,
+  reviews: "360 Reviews",
+  priceSale: "$840",
+  priceOriginal: "$840",
+  rating: 5,
+  tourPlans: [
+    {
+      title: "Day 1 Cairo",
+      des: `
 Pick-Up and Transfer:
 
-Begin your journey with a transfer from your hotel in Makadi Bay to Luxor in a private A/C vehicle. Board the 1st class train to Aswan. Upon arrival, meet your private Egyptologist tour guide and start your exploration.
-Visit the Aswan High Dam:
+Your representative will pick you up from your hotel in Sharm El Sheikh in a private air-conditioned vehicle for your flight to Cairo. Upon arrival, meet your Egyptologist guide to explore Cairo's attractions.
+Arrival in Cairo:
 
-Built in the 1960s and 1970s, the Aswan High Dam is one of the largest embankment dams in the world. It plays a crucial role in the economy and culture of Egypt.
-Explore the Unfinished Obelisk:
+Upon arrival at Cairo airport, meet your private tour guide who will accompany you throughout the day.
+Explore the Giza Pyramids Complex:
 
-This is the largest known obelisk from ancient Egypt, commissioned by Queen Hatshepsut (1508-1458 BC). The obelisk showcases the carving techniques of the ancient Egyptians and is now an open-air museum.
-Lunch Time:
+Discover the colossal art of the ancient Egyptians, including the three main pyramids, six little pyramids, the Great Sphinx, and the Valley Temple.
+The Great Pyramid:
 
-Enjoy lunch at a local restaurant in Aswan.
-Discover Philae Temple:
+Marvel at the Great Pyramid of Pharaoh Khufu, the last surviving wonder of the ancient world, standing tall at 147 meters with its intricate chambers.
+The Great Sphinx:
 
-Dedicated to the goddess Isis, Philae Temple was built in 280 BC during the reign of Ptolemy II. It was relocated to Agilkia Island by UNESCO to save it from the rising waters of the Nile.
-Check-in:
+Gaze upon the Great Sphinx, a magnificent limestone statue with the body of a lion and the head of Pharaoh Khafre, symbolizing wisdom and power.
+The Valley Temple:
 
-Transfer to a 5-star hotel in Aswan for an overnight stay.
+Visit the Valley Temple, a mortuary temple used for the mummification process of 4th dynasty pharaohs before their burial in the pyramids.
+Discover the Grand Egyptian Museum:
+
+Explore the latest addition to Egypt's rich cultural heritage, home to a vast collection of artifacts, including the Hanging Obelisk, the King Ramses II Statue, the 10 Statues of King Senusert, and more. (Note: A VIP ticket is available for additional access to exclusive artifacts.)
+Lunch at a Pyramid/Nile View Restaurant:
+
+Enjoy a delightful lunch with stunning views of the Pyramids or the Nile.
+Visit the Egyptian Museum:
+
+Tour the historic Egyptian Museum, showcasing an extensive collection of ancient Egyptian artifacts, located in the heart of Cairo since 1901 AD.
+Transfer to Cairo Airport:
+
+You will be driven to Cairo Airport for your flight to Luxor. Upon arrival, transfer to a 5-star hotel for an overnight stay.
 Meals:
 
 Lunch.
 Overnight:
 
-Aswan Hotel.
+Luxor Hotel.
+Flight:
+
+From Sharm to Cairo and From Cairo to Luxor.
 `,
-      },
-      {
-        title: "Day 2 Abu Simbel",
-        des: `
-Early Morning:
+    },
+    {
+      title: "Day 2 Luxor",
+      des: `
+Morning:
 
-Enjoy breakfast as you prepare for an early start.
-Visit the Abu Simbel Temples:
+Enjoy breakfast at your hotel, then your representative will pick you up from your hotel in a private air-conditioned vehicle to explore Luxor's attractions.
+Explore the Valley of the Kings:
 
-These temples are part of the UNESCO World Heritage Site known as the "Nubian Monuments." The temples, built by Pharaoh Ramses the Great in 1200 BC, honor Ptah, Amun, Ra, and commemorate Ramses' victory at the Battle of Kadesh and his devotion to Queen Nefertari.
+This sacred site on the west bank of the Nile opposite Luxor is a golden history book buried deep within a sacred hill. Home to 63 royal tombs of the greatest Pharaohs of the 18th, 19th, and 20th dynasties of the New Kingdom (1570–1050 BC), including Ramses II, Hatshepsut, Seti I, and Tutankhamen. The valley gained global fame with the discovery of Tutankhamen's tomb, making it one of the most famous archaeological sites on earth. It was declared a World Heritage Site in 1979, along with the entire Theban Necropolis.
+Visit Hatshepsut Temple:
+
+This beautiful temple, known as the "Holy of Holies," was constructed over 15 years on the golden rock hills of Deir El Bahari in 1479 BC. Built to honor the sun god Amun and immortalize the legacy of Queen Hatshepsut, one of the most successful pharaohs in ancient Egyptian history, it is the most preserved example of classical ancient Egyptian architecture.
+Admire the Colossi of Memnon:
+
+These two grand statues of Pharaoh Amenhotep III (1386 - 1349 BC) were built in 1350 BC to guard the entrance of his long-faded temple. The statues underwent numerous renovations and became known for their Vocal Phenomenon at dawn during the Greco-Roman period. The Colossi are adorned with mystical decorations depicting Amenhotep III's history, family, and the Nile god Hapi.
 Lunch Time:
 
-Return to Aswan for lunch before boarding the 1st class train back to Luxor.
-Return to Makadi Bay:
+Enjoy a delicious lunch on an island, accompanied by a scenic boat trip on the Nile River. After lunch, you will continue your guided tour to visit:
+Discover Karnak Temple:
 
-Transfer to your hotel, ending the Makadi Bay excursion to Aswan and Abu Simbel in two days.
+This colossal temple complex, covering 247 acres, was built between 2000 BC and 30 BC during the Middle Kingdom to the Greco-Roman period. It served as the main religious center for worshipping the Theban triad of Amun, Mut, and Khonsu for over 1500 years. The complex features the Avenue of Sphinxes, the grand Hypostyle Hall, and numerous carvings, paintings, and shrines dedicated to various gods.
+Explore Luxor Temple:
+
+The Luxor Temple, the southern Sanctuary, was created to be a center of celebration and admiration. Built in 1400 BC, it is dedicated to the Theban triad of Amun, Mut, and Khonsu and to host the Opet festival for the rejuvenation of kingship. It became part of the UNESCO World Heritage site in 1979.
+Transfer:
+
+Transfer to Luxor airport for your flight to Cairo, then catch your direct flight back to Sharm El Sheikh. Upon arrival, your representative will transfer you to your hotel in Sharm.
 Meals:
 
 Breakfast, Lunch.
+Flight:
+
+From Luxor to Cairo and From Cairo to Sharm.
 `,
-      },
-    ],
-    included: [
-      "Pick-up service from your hotel in Makadi Bay and return. ",
-      "2 lunch meals in Aswan.",
-      "All transfers by a private modern air-conditioned vehicle",
-      "Private Egyptologist guide during your tour.",
-      "Entrance fees to all the mentioned sights. ",
-      "One-night accommodation in Aswan at a 5-star Hotel. ",
-      "1st class Train tickets from Luxor to Aswan and return. ",
-      "Mineral water and soft drinks onboard the vehicle. ",
-      "All service charges and taxes. ",
-    ],
-    excluded: [
-      "Any extras not mentioned in the program. ",
-      "Tipping",
-      "Optional Experiences available at an additional cost. ",
-    ],
+    },
+  ],
+  included: [
+    "Pick-up service from your hotel in Sharm El Sheikh and return. ",
+    "Domestic flight tickets from Sharm to Cairo/ Cairo to Luxor/ Luxor to Cairo/ Cairo to Sharm. ",
+    "All transfers by a private modern air-conditioned vehicle. ",
+    "Private Egyptologist guide during your tour. ",
+    "Entrance fees to all the mentioned sights. ",
+    "One night accommodation at a 5-star hotel in Luxor. ",
+    "2 Lunch meals during tours in Cairo and Luxor. ",
+    "Mineral water and soft drinks onboard the vehicle. ",
+    "All service charges and taxes. ",
+  ],
+  excluded: [
+    "Any extras not mentioned in the program. ",
+    "Tipping",
+    "Optional Experiences available at an additional cost.",
+  ],
 };
 
 fs.readFile(path, "utf8", (err, data) => {
@@ -175,7 +205,7 @@ fs.readFile(path, "utf8", (err, data) => {
   // Define a new filename
   let newFileName = Data.title.replace(/ /g, "-"); // Replace all spaces with hyphens
 
-  const newPath = `./Egypt-Day-Tours-Makadi-Bay-Excursions/${newFileName}.html`;
+  const newPath = `./Egypt-Day-Tours-Sharm-Elshekh-Excustions/${newFileName}.html`;
   // Write the updated content to a new HTML file
   fs.writeFile(newPath, updatedData, "utf8", (err) => {
     if (err) {
